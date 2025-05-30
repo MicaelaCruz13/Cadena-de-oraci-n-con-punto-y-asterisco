0eb5f2eb96e9bea4c67376c645e66a282cefa81b
MicaelaCruz13 <micacruz1304@gmail.com>
2025-05-30 11:18:28 -0300

    Add files via upload

---
+++ Cadena con uso de estructura struct.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+#define nro 5
+
+struct cuentas
+{
+	int nro_cuenta;
+	char nombre[30];
+	int saldo;
+};
+
+int main()
+{
+	struct cuentas cuen[nro];
+	int i, deudor=0,acreedor=0;
+	
+	for (i=0; i < nro;i++)
+	{
+		printf("Ingrese Nro de Cuenta\n");
+		scanf("%i",&cuen[i].nro_cuenta);
+		
+		printf("Ingrese nombre del cliente\n");
+		scanf("%s", &cuen[i].nombre);
+		
+		printf("Ingrese saldo del cliente\n");
+		scanf ("%d",&cuen[i].saldo);
+		printf("\n");
+	}
+	
+	return 0;
+}
